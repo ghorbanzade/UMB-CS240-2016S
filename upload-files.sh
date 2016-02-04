85b83669f8c65f06b6547f023f1f2794015ccc84
Pejman Ghorbanzade <mail@ghorbanzade.com>
2016-02-04 11:53:52 -0500

    support different destination filename for upload script

--- upload-files.sh
+++ upload-files.sh
@@ -17,8 +17,14 @@ print_put_statements_from_file () {
 	dst_dir="bin/courses/CS240-2016S/"
 	while IFS='' read -r line || [[ -n "$line" ]]; do
 		if [ ! -z "$line" ] && [ ${line::1} != '#' ]; then
-			src_path=$src_dir$line
-			dst_path=$dst_dir$line
+			line=($line)
+			if [ ${#line[@]} == 2 ]; then
+				src_path=$src_dir${line[0]}
+				dst_path=$dst_dir${line[1]}
+			else
+				src_path=$src_dir$line
+				dst_path=$dst_dir$line
+			fi
 			echo "put $src_path $dst_path"
 		fi
 	done < "$1"
