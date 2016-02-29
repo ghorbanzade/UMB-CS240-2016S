9554a6d076aad9dbbdb675c254068e1a09a6e77b
Pejman Ghorbanzade <mail@ghorbanzade.com>
2016-02-29 13:41:01 -0500

    added lecture slide 9

---
+++ src/main/c/res/ls09-header.h
@@ -0,0 +1,19 @@
+/**
+ * CS240: Programming in C
+ * Copyleft 2016 Pejman Ghorbanzade <mail@ghorbanzade.com>
+ * More info: https://github.com/ghorbanzade/UMB-CS240-2016S
+ *
+ * The author has placed this file in the public domain.
+ * He makes no warranty and accepts no liability for this file.
+ */
+
+#ifndef LS09_HEADER_H
+#define LS09_HEADER_H
+
+#include <stdio.h>
+
+#define NUM_TIMES 10
+
+void say_hello(int i);
+
+#endif // LS09_HEADER_H
