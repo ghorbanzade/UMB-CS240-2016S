3b248f7ee9cea186e9ea6ab39bfa8f876a202566
Pejman Ghorbanzade <mail@ghorbanzade.com>
2016-04-08 01:22:39 -0400

    added lecture slide 15

---
+++ src/main/c/res/ls15/fileio-r3.c
@@ -0,0 +1,24 @@
+/**
+ * CS240: Programming in C
+ * Copyleft 2016 Pejman Ghorbanzade <mail@ghorbanzade.com>
+ * More info: https://github.com/ghorbanzade/UMB-CS240-2016S
+ *
+ * The author has placed this file in the public domain.
+ * He makes no warranty and accepts no liability for this file.
+ */
+
+#include <stdio.h>
+#define MAXLEN 16
+
+int main(void)
+{
+	int len;
+	FILE *fp;
+	char buff[MAXLEN];
+	fp = fopen("sample.txt", "r");
+	while (len = fread(buff, sizeof(char), MAXLEN, fp)) {
+		buff[len] = '\0';
+		printf("%s", buff);
+	}
+	fclose(fp);
+}
---
+++ src/main/c/res/ls15/fileio-w.c
@@ -0,0 +1,18 @@
+/**
+ * CS240: Programming in C
+ * Copyleft 2016 Pejman Ghorbanzade <mail@ghorbanzade.com>
+ * More info: https://github.com/ghorbanzade/UMB-CS240-2016S
+ *
+ * The author has placed this file in the public domain.
+ * He makes no warranty and accepts no liability for this file.
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	FILE *fp;
+	fp = fopen("sample.txt", "w");
+	fprintf(fp, "content of the file\n");
+	fclose(fp);
+}
