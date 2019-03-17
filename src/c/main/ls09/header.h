f9120f213b784778df46c6aa661ba71117872092
Pejman Ghorbanzade <pejman@ghorbanzade.com>
2019-03-17 15:53:29 -0500

    updated license header block in all source files

--- src/c/main/ls09/header.h
+++ src/c/main/ls09/header.h
@@ -1,11 +1,11 @@
 /**
- * CS240: Programming in C
- * Copyleft 2016 Pejman Ghorbanzade <pejman@ghorbanzade.com>
+ * UMB-CS240-2016S: Programming in C
+ * Copyright 2016 Pejman Ghorbanzade <pejman@ghorbanzade.com>
  * More info: https://github.com/ghorbanzade/UMB-CS240-2016S
- *
- * The author has placed this file in the public domain.
- * He makes no warranty and accepts no liability for this file.
  */
+
+
+
 
 #ifndef LS09_HEADER_H
 #define LS09_HEADER_H
